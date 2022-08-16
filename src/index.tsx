57180f3133fd86a40d2c7da8327413a4a150483f
Charleston Amaral <charleston.silva03@gmail.com>
2022-08-15 21:57:02 -0300

    feat: add function valid cpf

--- src/index.tsx
+++ src/index.tsx
@@ -192,4 +192,44 @@ function useMercadoPago({ publicKey, locale = 'pt-BR', setError }: UseMercadoPag
     };
 }
 
-export { useMercadoPago };
+function isValidCPF(strCPF: string) {
+    var Soma;
+    var Resto;
+
+    Soma = 0;
+    strCPF = strCPF.replace(/[^\d]+/g, '');
+
+    for (let i = 1; i <= 9; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    }
+
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) {
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(strCPF.substring(9, 10))) {
+        return false;
+    }
+
+    Soma = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    }
+
+    Resto = (Soma * 10) % 11;
+
+    if (Resto == 10 || Resto == 11) {
+        Resto = 0;
+    }
+
+    if (Resto != parseInt(strCPF.substring(10, 11))) {
+        return false;
+    }
+
+    return true;
+}
+
+export { useMercadoPago, isValidCPF };
